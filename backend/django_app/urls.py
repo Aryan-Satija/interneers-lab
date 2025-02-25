83fa5caa606c8df667ad5abee48784598c42a892
aryan <aryansatija2003@gmail.com>
2025-02-25 14:09:22 +0530

    feat: hello_name django api

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import JsonResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
-
+def hello_name(request):
+    """
+    A simple view that returns 'Hello, {name}' in JSON format.
+    Uses a query parameter named 'name'.
+    """
+    
+    name = request.GET.get("name", "world")
+    
+    return JsonResponse({
+        "status": True,
+        "message": f"Hello {name}"
+    })
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/', hello_name),
 ]
