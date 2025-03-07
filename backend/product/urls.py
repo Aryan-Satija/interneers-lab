a1cb7fe6abf3b7969f79871273c0b2502318e3fc
aryan <aryansatija2003@gmail.com>
2025-03-08 00:03:40 +0530

    feat: add addProduct

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('getall', views.getProductList),
+    path('add', views.addProduct)
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -9,3 +9,20 @@ def getProductList(request):
     products = Product.objects.all()   
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def addProduct(request):
+    category_id = request.data.get('category')
+    brand_id = request.data.get('brand')
+    
+    if not Category.objects.filter(id=category_id).exists():
+        return Response({"error": "Category does not exist"})
+
+    if not Brand.objects.filter(id=brand_id).exists():
+        return Response({"error": "Brand does not exist"})
+    
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
