97a19a002dddb489c478cb5370c3a44cc5c923cf
aryan <aryansatija2003@gmail.com>
2025-03-16 13:45:12 +0530

    feat: add serializers for Products, Category, and Brand

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,8 +1,35 @@
-from rest_framework import serializers
-from .models import Product
+from rest_framework_mongoengine.serializers import DocumentSerializer
+import rest_framework.serializers as serializers
+from .models import Products, Category, Brand
+from bson import ObjectId
 
-class ProductSerializer(serializers.ModelSerializer):
+class ObjectIdField(serializers.Field):
+    def to_internal_value(self, data):
+        try:
+            return ObjectId(data)
+        except:
+            raise serializers.ValidationError("Invalid ObjectId")        
+    
+    def to_representation(self, value):
+        return str(value)
+    
+class BrandSerializer(DocumentSerializer):
+    
     class Meta:
-        model = Product
-        fields = '__all__' 
+        model = Brand
+        fields = '__all__'
         
+class CategorySerializer(DocumentSerializer):
+    
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductsSerializer(DocumentSerializer):
+    
+    brand = ObjectIdField()
+    category = ObjectIdField()
+    
+    class Meta:
+        model = Products
+        fields = '__all__'
