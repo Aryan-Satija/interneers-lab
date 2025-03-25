2380fd2305ff730e29348720dca8f36a1d55db1c
aryan <aryansatija2003@gmail.com>
2025-03-25 20:13:23 +0530

    feat: add field validations for product model

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -33,3 +33,24 @@ class ProductsSerializer(DocumentSerializer):
     class Meta:
         model = Products
         fields = '__all__'
+        
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price cannot be negative")
+        return value
+    
+    def validate_quantity(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Quantitiy cannot be negative")
+        return value
+    
+    def validate_brand(self, value):
+        if not Brand.objects(id = value).first():
+            raise serializers.ValidationError("Brand doesn't exist")
+        return value
+
+    def validate_category(self, value):
+        if not Category.objects(id = value).first():
+            raise serializers.ValidationError("Category doesn't exist")
+        return value
+    
