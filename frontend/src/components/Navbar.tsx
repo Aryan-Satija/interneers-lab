bdfd4309014112c03f76a95011af42338d9c368f
aryan <aryansatija2003@gmail.com>
2025-04-28 15:44:50 +0530

    feat: add tailwindcss setup

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="w-full p-4 fixed top-0 left-0 flex flex-row">
+      <div className="font-bold">Interneers' lab</div>
+    </div>
+  );
+}
+
+export default Navbar;
